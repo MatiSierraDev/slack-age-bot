e50c766a446c84cfba2c252612ae4ccf9ea2ab1a
Matias Sierra <matisierra.develop@gmail.com>
2023-07-11 17:47:32 -0300

    Add environment and connection for slacker client on main.go file

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
 )
 
 func main() {
+	godotenv.Load(".env")
+	SLACK_BOT_TOKEN := os.Getenv("SLACK_BOT_TOKEN")
+	SLACK_APP_TOKEN := os.Getenv("SLACK_APP_TOKEN")
+	os.Setenv("SLACK_BOT_TOKEN", SLACK_BOT_TOKEN)
+	os.Setenv("SLACK_APP_TOKEN", SLACK_APP_TOKEN)
 
-	fmt.Printf("Hi!")
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
+	bot.Command("ping", &slacker.CommandDefinition{
+		Handler: func(ctx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			response.Reply("pong!")
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := bot.Listen(ctx); err != nil {
+		panic(err.Error())
+	}
 }
